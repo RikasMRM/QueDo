58b588bc345967bec2bcd608929dc0472023c857
Shadurshan S <67456320+Shadurshan1229@users.noreply.github.com>
2020-10-04 23:16:47 +0530

    QuickNote

---
+++ app/src/main/java/com/example/quedo/Note.java
@@ -0,0 +1,25 @@
+package com.example.quedo;
+
+public class Note {
+
+    private String title;
+    private String desc;
+
+    public Note() {}
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}
