9e2722535a08411fc2b14cb70b3e2e14cc478e74
RikasMRM <rikashrkf@gmail.com>
2020-10-16 10:21:07 +0530

    Tasks Final Update
    made some changes in validation

--- app/src/main/java/com/example/quedo/AddNewTask.java
+++ app/src/main/java/com/example/quedo/AddNewTask.java
@@ -14,19 +14,25 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.basgeekball.awesomevalidation.AwesomeValidation;
+import static com.basgeekball.awesomevalidation.utility.RegexTemplate.*;
+
+import com.basgeekball.awesomevalidation.ValidationStyle;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.Random;
 
+
 public class AddNewTask extends Fragment {
 
-    EditText mtask, description, date;
+    EditText mtask, mdescription, mdate;
     Button savebutton , cancelbutton;
 
     private DatabaseReference reference;
     private FirebaseAuth mAuth;
+    private AwesomeValidation awesomeValidation;
 
     @Nullable
     @Override
@@ -35,32 +41,20 @@ public class AddNewTask extends Fragment {
         View rootView = inflater.inflate(R.layout.todo_addnewtask, container, false);
 
        mtask = rootView.findViewById(R.id.task);
-       description = rootView.findViewById(R.id.description);
-       date = rootView.findViewById(R.id.date);
+       mdescription = rootView.findViewById(R.id.description);
+       mdate = rootView.findViewById(R.id.date);
        savebutton = rootView.findViewById(R.id.saveBtn);
        cancelbutton = rootView.findViewById(R.id.CancelBtn);
 
         mAuth = FirebaseAuth.getInstance();
         reference = FirebaseDatabase.getInstance().getReference();
 
-
-        final Random random = new Random();
-
-
+        awesomeValidation = new AwesomeValidation(ValidationStyle.BASIC);
 
         savebutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String ntask = mtask.getText().toString();
-                String ndescription = description.getText().toString();
-                String ndate = date.getText().toString();
-
-                Task task =  new Task(ntask, ndescription, ndate);
-                reference.child("Tasks").child(Integer.toString(random.nextInt(100))).setValue(task);
-                Toast.makeText(AddNewTask.this.getActivity(), "Task has been inserted successfully", Toast.LENGTH_SHORT).show();
-
-                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                        new AllTasks()).addToBackStack(null).commit();
+                validateForm();
 
             }
         });
@@ -80,5 +74,37 @@ public class AddNewTask extends Fragment {
         return rootView;
     }
 
+//    @Override
+//    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+//        super.onActivityCreated(savedInstanceState);
+//
+//        //Validation 1st time
+//        awesomeValidation.addValidation(AddNewTask.this.getActivity(), R.id.task,"^[A-Za-z\\s]{1,}[\\.]{0,1}[A-Za-z\\s]{0,}$",  R.string.thisFieldError);
+//        awesomeValidation.addValidation(AddNewTask.this.getActivity(), R.id.description, NOT_EMPTY,  R.string.thisFieldError);
+//        awesomeValidation.addValidation(AddNewTask.this.getActivity(), R.id.date, NOT_EMPTY,  R.string.thisFieldError);
+//    }
 
+    private void validateForm() {
+        String ntask = mtask.getText().toString();
+        String ndescription = mdescription.getText().toString();
+        String ndate = mdate.getText().toString();
+
+        //Validation
+        awesomeValidation.addValidation(AddNewTask.this.getActivity(), R.id.task,"^[A-Za-z\\s]{1,}[\\.]{0,1}[A-Za-z\\s]{0,}$",  R.string.thisFieldError);
+        awesomeValidation.addValidation(AddNewTask.this.getActivity(), R.id.description, NOT_EMPTY,  R.string.thisFieldError);
+        awesomeValidation.addValidation(AddNewTask.this.getActivity(), R.id.date, NOT_EMPTY,  R.string.thisFieldError);
+
+        Task task =  new Task(ntask, ndescription, ndate);
+        final Random random = new Random();
+
+        if (awesomeValidation.validate()) {
+            //insert to Database
+            reference.child("Tasks").child(Integer.toString(random.nextInt(100))).setValue(task);
+            Toast.makeText(AddNewTask.this.getActivity(), "Task has been inserted successfully", Toast.LENGTH_SHORT).show();
+            getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new AllTasks()).addToBackStack(null).commit();
+        }
+    }
 }
+
+
